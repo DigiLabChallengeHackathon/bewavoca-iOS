5e169d60e3323e971bc4ef78625387e451edd4b4
Moo <mutopia82@gmail.com>
2024-12-28 22:42:41 +0900

    feat: OnboardingView1 애니메이션 추가 #33
    눈뜨는듯한 애니메이션 구현

--- bewavoca-iOS/bewavoca-iOS/Sources/Presentation/Onboarding/Views/OnboardingView1.swift
+++ bewavoca-iOS/bewavoca-iOS/Sources/Presentation/Onboarding/Views/OnboardingView1.swift
@@ -1,6 +1,8 @@
 import SwiftUI
 
 struct OnboardingView1: View {
+    @State private var isEyeOpened = false
+    
     var body: some View {
         ZStack {
             Image(OnboardingPage.page1.backgroundImage)
@@ -11,6 +13,35 @@ struct OnboardingView1: View {
             Image(OnboardingPage.page1.characterImage)
                 .frame(width: 1025)
                 .offset(x: 450, y: 130)
+            
+            // 눈 뜨는 애니메이션
+            EyeOpeningAnimation(isOpened: isEyeOpened)
+        }
+        .onAppear {
+            withAnimation(.easeInOut(duration: 3)) {
+                isEyeOpened = true
+            }
+        }
+    }
+}
+
+// MARK: - Eye Opening Animation View
+private struct EyeOpeningAnimation: View {
+    let isOpened: Bool
+    
+    var body: some View {
+        ZStack {
+            // 위쪽 검정색 뷰
+            Color.black
+                .frame(height: UIScreen.main.bounds.height)
+                .frame(maxHeight: .infinity, alignment: .top)
+                .offset(y: isOpened ? -UIScreen.main.bounds.height : 0)
+            
+            // 아래쪽 검정색 뷰
+            Color.black
+                .frame(height: UIScreen.main.bounds.height)
+                .frame(maxHeight: .infinity, alignment: .bottom)
+                .offset(y: isOpened ? UIScreen.main.bounds.height : 0)
         }
     }
 }
