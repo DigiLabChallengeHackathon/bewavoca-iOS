dba04ea968f0b88a0edb85c70dd16e9a4989edba
Moo <mutopia82@gmail.com>
2025-02-01 19:53:55 +0900

    feat: 회원가입 DTO 추가 및 API 응답 구조체 리팩토링 #54
    APIResponse 공통 응답 구조체 추가, UserDataResponse, SignUpResponse 추가

---
+++ bewavoca-iOS/bewavoca-iOS/Sources/Data/DTOs/UserDTO.swift
@@ -0,0 +1,20 @@
+import Foundation
+
+struct APIResponse<T: Decodable>: Decodable {
+    let status: String
+    let message: String
+    let data: T?
+}
+
+struct UserDataResponse: Decodable {
+    let userid: Int
+    let nickname: String
+    let character: Int
+    let region: Int
+    let level: Int
+}
+
+struct SignUpResponse: Decodable {
+    let userId: Int
+    let nickname: String
+}
